e68f88ec79dee75dde19d3ac037e8f95bab5e824
Михайло Козьма <michailkozma05@gmail.com>
2024-07-15 11:12:25 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('Server started!');
+});
