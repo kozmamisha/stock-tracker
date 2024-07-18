d170e902dc1a16653879f4cd844eebb1db3bd1a6
Михайло Козьма <michailkozma05@gmail.com>
2024-07-18 14:54:18 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  dotenv.config()   // config for corrent work env variables
+  dotenv.config(); // config for corrent work env variables
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(4444);
 }
 bootstrap();
